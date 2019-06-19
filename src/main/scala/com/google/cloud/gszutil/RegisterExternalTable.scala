23c82a1f2de52e68ab25fcd2897bdce57ac5cd3a
Jason Mar <jasonmar@google.com>
2019-06-19 17:22:13 -0400

    update copyright

---
+++ src/main/scala/com/google/cloud/gszutil/RegisterExternalTable.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.gszutil
+
+import com.google.cloud.bigquery.{BigQuery, ExternalTableDefinition, FormatOptions, Table, TableId, TableInfo}
+
+object RegisterExternalTable {
+  def run() = {
+
+  }
+
+  def defaultExpiration: Long = System.currentTimeMillis() + 1000*60*60*6 // 6 hours
+
+  def create(bq: BigQuery, tableId: TableId, sources: Seq[String]): Table = {
+    import scala.collection.JavaConverters.seqAsJavaListConverter
+    val tableDefinition = ExternalTableDefinition
+      .newBuilder(sources.asJava, null, FormatOptions.orc())
+      .build()
+
+    val tableInfo = TableInfo
+      .newBuilder(tableId, tableDefinition)
+      .setExpirationTime(defaultExpiration)
+      .build()
+
+    bq.create(tableInfo)
+  }
+}
